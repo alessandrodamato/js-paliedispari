aae6fbb51457835275d0dcb04e0f4ce3e8eacade
alessandrodamato <alessandro042004@gmail.com>
2024-02-28 16:31:09 +0100

    [add] pari e dispari funzionante

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 ///////////////// PALINDROMA /////////////////
 
-const userWord = 'giovanni'; //prompt('Inserisci una parola palindroma')
+const userWord = prompt('Inserisci una parola palindroma');
 const reverseOutput = document.getElementById('reverse');
 reverseOutput.innerHTML = palindromaCheck();
 
@@ -9,18 +9,46 @@ reverseOutput.innerHTML = palindromaCheck();
 const btnGo = document.getElementById('go');
 const numberPlayer = document.getElementById('num-player');
 const choose = document.getElementById('choose');
+const numbers = document.getElementById('numbers');
 let playerChoose;
+let pcChoose;
+let winCondition;
 
 btnGo.addEventListener('click', function(){
+
+  playerChoose = parseInt(numberPlayer.value);
+  pcChoose = parseInt(getRandomNumber(1, 5));
   
-  if ((choose.value === 'pari') && (numberPlayer.value > 0) && (numberPlayer.value <= 5)){
-    console.log('Scegli pari');
+  if ((choose.value == 'pari') && (numberPlayer.value > 0) && (numberPlayer.value <= 5)){
     
-  } else if ((choose.value === 'dispari') && (numberPlayer.value > 0) && (numberPlayer.value <= 5)){
-    console.log('Scegli dispari');
+    numbers.innerHTML = `
+    <div>PC: ${pcChoose} | Player: ${playerChoose}</div>
+    <div>Somma = ${sum(playerChoose, pcChoose)}</div>
+    `;
+    
+    if (winCondition === 'PARI'){
+      numbers.innerHTML += 'Vince il Player';
+    } else {
+      numbers.innerHTML += 'Vince il PC';
+    }
+    
+  } else if ((choose.value == 'dispari') && (numberPlayer.value > 0) && (numberPlayer.value <= 5)){
+
+    numbers.innerHTML = `
+    <div>PC: ${pcChoose} | Player: ${playerChoose}</div>
+    <div>Somma = ${sum(playerChoose, pcChoose)}</div>
+    `;
+
+    if (winCondition === 'DISPARI'){
+      numbers.innerHTML += 'Vince il Player';
+    } else {
+      numbers.innerHTML += 'Vince il PC';
+    }
   
   } else {
-    console.log('Scegli tra pari e dispari e inserisci un numero da 1 a 5');
+
+    numbers.innerHTML = 'Devi scegliere tra pari e dispari e inserire un numero da 1 a 5 per poter giocare';
+
   }
 
 })
@@ -50,6 +78,15 @@ function palindromaCheck () {
   }
 }
 
-function getRandomNumber () {
-  
+function getRandomNumber (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function sum (numA, numB) {
+
+  if ((numA + numB) % 2 === 0) {
+   return winCondition = 'PARI'
+  }
+  return winCondition = 'DISPARI'
+
 }
